0b475a0902d61c0cb1b881ba94d9a7ce2534806b
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-04-25 15:36:31 -0300

    findAll Technical

--- src/main/java/com/alex/helpdesk/resources/TechnicalResource.java
+++ src/main/java/com/alex/helpdesk/resources/TechnicalResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.lang.Long;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.alex.helpdesk.domain.dto.TechnicalDTO;
 import com.alex.helpdesk.domain.entities.Technical;
@@ -23,6 +25,13 @@ public class TechnicalResource {
 	public ResponseEntity<TechnicalDTO> findById(@PathVariable Long id) {
 		Technical obj = service.findById(id);
 		return ResponseEntity.ok().body(new TechnicalDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TechnicalDTO>> findAll(){
+		List<Technical> list = service.findAll();
+		List<TechnicalDTO> listDTO = list.stream().map(obj -> new TechnicalDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
--- src/main/java/com/alex/helpdesk/services/TechnicalService.java
+++ src/main/java/com/alex/helpdesk/services/TechnicalService.java
@@ -27,4 +27,6 @@ public class TechnicalService {
 		return repository.findAll();
 	}
 	
+	
+	
 }
